27bd911fb8510dae703b635c16e0fa74e1fd8c83
Wilson Alfonso Gomez Santiago <wilsonscout20@gmail.com>
2022-12-30 12:02:06 -0400

    Fixing the navbar colors, adding an image to the assetss and adding that the github button in the work section don't appears if it doesn't have a link.

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -86,16 +86,20 @@ const Work = () => {
                   </motion.div>
                 </a>
                 
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
-                    <motion.div
-                      whileInView={{ scale: [0 , 1] }}
-                      whileHover={{ scale: [1 , 0.9] }}
-                      transition={{ duration: 0.25}}
-                      className="app__flex"
-                    >
-                      <AiFillGithub/>
-                    </motion.div>
-                  </a>
+                {
+                  (work.codeLink !== undefined || work.codeLink?.trim().length > 0 ) && (
+                      <a href={work.codeLink} target="_blank" rel="noreferrer">
+                      <motion.div
+                        whileInView={{ scale: [0 , 1] }}
+                        whileHover={{ scale: [1 , 0.9] }}
+                        transition={{ duration: 0.25}}
+                        className="app__flex"
+                      >
+                        <AiFillGithub/>
+                      </motion.div>
+                    </a>
+                    )
+                }
               </motion.div>
             </div>
 
